3dc52945265017103d3d0f54dd4e7a662cadde1b
Grzegorz Kossakowski <grzegorz.kossakowski@gmail.com>
2012-08-02 16:27:53 +0200

    Update single rev page.
    A few changes:
      * formatting of `rev.scala.html`
      * switch to `CompilerRevBenchmarks`
      * add link to github

--- profiling-viewer/app/controllers/SingleCompilerRev.scala
+++ profiling-viewer/app/controllers/SingleCompilerRev.scala
@@ -6,7 +6,8 @@ import play.api.mvc._
 object SingleCompilerRev extends Controller {
   
   def rev(sha1: String) = Action {
-    Ok(views.html.rev(sha1, Stats.benchmarks(sha1)))
+    val data = models.CompilerRevBenchmarks(GitUtils.compilerRev(sha1), Stats.benchmarks(sha1))
+    Ok(views.html.rev(data))
   }
 
 }
