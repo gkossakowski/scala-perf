a2dff5b64b19475ab011091658c39d4777d3eaf0
Grzegorz Kossakowski <grzegorz.kossakowski@gmail.com>
2012-08-02 14:22:30 +0200

    Add jgit as dependency.
    We'll be using more of jgit APIs instead of
    calling git command line.

--- profiling-viewer/project/Build.scala
+++ profiling-viewer/project/Build.scala
@@ -10,7 +10,8 @@ object ApplicationBuild extends Build {
     val appDependencies = Seq(
       // Add your project dependencies here,
       "com.github.scala-incubator.io" %% "scala-io-core" % "0.4.0",
-      "com.github.scala-incubator.io" %% "scala-io-file" % "0.4.0"
+      "com.github.scala-incubator.io" %% "scala-io-file" % "0.4.0",
+      "org.eclipse.jgit" % "org.eclipse.jgit" % "2.0.0.201206130900-r"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
