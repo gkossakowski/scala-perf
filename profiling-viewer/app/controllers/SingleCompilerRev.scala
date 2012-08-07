0498ed26fb154416da02851aca23832a69169765
Grzegorz Kossakowski <grzegorz.kossakowski@gmail.com>
2012-08-07 11:20:14 +0200

    Display performance graphs on single rev page.
    We display performance graphs for single commit
    in a way that tries to visualize how we got
    to the performance of given commit.
    
    We do that by having a graph of commits consisting
    of all ancestors of a given commit obtained by
    following the first parent.

--- profiling-viewer/app/controllers/SingleCompilerRev.scala
+++ profiling-viewer/app/controllers/SingleCompilerRev.scala
@@ -7,7 +7,19 @@ object SingleCompilerRev extends Controller {
   
   def rev(sha1: String) = Action {
     val data = models.CompilerRevBenchmarks(GitUtils.compilerRev(sha1), Stats.benchmarks(sha1))
-    Ok(views.html.rev(data, GitUtils.children(sha1)))
+
+    val revs = GitUtils.revList(sha1).takeRight(60)
+    val benchmarkNames = List("scalap-src", "Vector_scala", "Test_scala")
+    val allBenchmarks: Seq[models.CompilerRevBenchmarks] = for (rev <- revs) yield {
+      models.CompilerRevBenchmarks(GitUtils.compilerRev(rev), Stats.benchmarks(rev))
+    }
+    def avgsFor(name: String): Seq[Option[Double]] = for (benchmarkSet <- allBenchmarks.map(_.benchmarks)) yield {
+      val benchmark = benchmarkSet.find(_.name == name)
+      benchmark.map(_.wallclock.meanLowCov.value)
+    }
+    val avgs = benchmarkNames.map(name => name -> avgsFor(name)).toMap
+
+    Ok(views.html.rev(data, GitUtils.children(sha1), allBenchmarks, avgs))
   }
 
 }
